9bcf3e26f38203915f366c6a6b18fe92161bd35c
chris-jt <chtronconi@gmail.com>
2024-09-17 10:47:33 -0300

    mejoras script

--- 00_ec2/ec2_user_data.sh
+++ 00_ec2/ec2_user_data.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-echo "Installing Unzip"
-sudo apt install unzip
-unzip -v
+set -e  # Exit immediately if a command exits with a non-zero status
 
-# Actualizar e instalar dependencias
+echo "Installing Unzip"
 sudo apt-get update
-sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+sudo apt-get install -y unzip
+unzip -v
 
 echo "Installing AWS CLI"
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
@@ -14,43 +13,39 @@ sudo ./aws/install --update
 aws --version
 
 echo "Installing kubectl"
+sudo apt-get install -y kubectl
 curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.30.2/2024-07-12/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
-#echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
+echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
 kubectl version --client
 
-echo "Installing ekctl"
-# Download EKS CLI https://github.com/weaveworks/eksctl
-curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+echo "Installing eksctl"
+curl --silent --location "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 sudo mv /tmp/eksctl /usr/local/bin
-export PATH=$PATH:/usr/local/bin
-#echo 'export PATH=$PATH:/usr/local/bin' >> ~/.bashrc
 eksctl version
 
-echo "Installing docker"
-sudo snap install docker
-sudo addgroup --system docker
-sudo adduser $USER docker
-sudo usermod -a -G docker ec2-user
+echo "Installing Docker"
+sudo apt-get install -y docker.io
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo usermod -aG docker $USER
 newgrp docker
-wget https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) 
-sudo mv docker-compose-$(uname -s)-$(uname -m) /usr/local/bin/docker-compose
-sudo chmod -v +x /usr/local/bin/docker-compose
-sudo systemctl enable docker.service
-sudo systemctl start docker.service
+
+echo "Installing Docker Compose"
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
 
 echo "Installing Helm"
 curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
-chmod 700 get_helm.sh
-./get_helm.sh
 helm version
 
-# Instalar Prometheus y Grafana (opcional, para monitoreo)
+# Add Prometheus Helm repository
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
 
-echo "Todas las herramientas necesarias han sido instaladas."
+echo "All necessary tools have been installed."
 
 
 
